5c144f2d7847dd518d660c30c64f83bfd560d2fb
David Jose Ferreira do Nascimento <70581471+David-DN715@users.noreply.github.com>
2021-04-21 14:36:56 -0300

    Update mylitlle_game.py

--- mylitlle_game.py
+++ mylitlle_game.py
@@ -1,7 +1,10 @@
 continuar = True
+# o import abaixo nos permite usar a função random!
 import random
+#Aqui começa o codigo principal!
 while continuar:
   comecar = input("deseja começar a rolar o dado?")=="sim"
+  #O print abaixo permite realizar o random nos numero dos dados e imprimir para o usuario!
   print("Dado lançado!\ndado nº\t", random.randint(1, 6))
   continuar = input("deseja continuar a rolar os dados?\n")== "sim"
   if continuar == 'não':
